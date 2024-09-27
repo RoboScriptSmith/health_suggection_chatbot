8cb5276449ad09c5c428f1ecaaaa59877b0cb90e
Aditya Saini <119171617+RoboScriptSmith@users.noreply.github.com>
2024-09-27 18:14:41 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -37,4 +37,10 @@ if submit and input:
     st.subheader("The Response is")
     for chunk in response:
         st.write(chunk.text)
+         st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
+
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
+
 
